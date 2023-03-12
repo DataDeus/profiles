ab2a59f99412a6902c9751cfd5bb299d4c940287
Eliel Godsent <godsenteliel@gmail.com>
2023-03-12 16:31:22 +0100

    Delete web directory

--- web/index.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
-
-hamburger.addEventListener('click', function ()  {
-    navLink.classList.toggle('hide');
-});
